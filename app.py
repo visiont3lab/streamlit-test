98bc610cc75a678d5722024359004880e6d58180
Manuel Rucci <ruccimanuel7@gmail.com>
2022-10-05 19:22:00 +0200

    aggiunto i dati di input

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import streamlit as st
+import numpy as np
+import plotly.graph_objects as go 
 
-st.title("Dashboard Manuel")
+a = 0.1*np.random.random(1000)
+b = -0.1*np.random.random(1000)
+X1  = np.linspace(0,20,1000)
+Y = np.cos(X1)*np.exp(-0.2*X1) +a + b
+Y = Y.reshape(-1,1)
+fig = go.Figure()
+fig.add_traces( go.Scatter( x=X1,y=Y[:,0], mode="markers", name="res" ))
+fig.update_layout(
+    title="Data",
+    hovermode="x"
+)
 
-test = st.button("Test")
+st.title("Dashboard Regressione lineare")
 
-if test:
-    st.text("Ho premuto iao")
-else:
-    st.text("Ciao")
+st.plotly_chart(fig, use_container_width=True)
